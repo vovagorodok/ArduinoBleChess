60f98ac1bc925f3a97f6b6080c89125dfbe98edc
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2022-05-26 00:56:46 +0200

    Private onWrite

--- lib/ArduinoBleChess/ArduinoBleChessEsp32.h
+++ lib/ArduinoBleChess/ArduinoBleChessEsp32.h
@@ -8,10 +8,10 @@ public:
     bool begin(const std::string &deviceName, BleChessDevice& device);
     bool begin(NimBLEServer* server, BleChessDevice& device);
 
-    void onWrite(BLECharacteristic* characteristic) override;
     void send(const std::string& str);
 
 private:
+    void onWrite(BLECharacteristic* characteristic) override;
     BLECharacteristic* txCharacteristic;
 };
 
