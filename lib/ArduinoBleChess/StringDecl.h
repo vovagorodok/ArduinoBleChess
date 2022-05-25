5b6f26b7c411d4a1d047f725e40da5adf8e81343
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2022-05-25 15:00:04 +0200

    String functions as inline

--- lib/ArduinoBleChess/StringDecl.h
+++ lib/ArduinoBleChess/StringDecl.h
@@ -12,8 +12,52 @@ using StringDecl = std::string;
 using StringDecl = String;
 #endif
 
-bool startsWith(const StringDecl& str, const StringDecl& start);
-StringDecl substring(const StringDecl& str, unsigned int beginIndex);
-StringDecl substring(const StringDecl& str, unsigned int beginIndex, unsigned int endIndex);
-int indexOf(const StringDecl& str, char match);
-int indexOf(const StringDecl& str, const StringDecl& match);
+#if __cpp_lib_starts_ends_with
+#error "std::string::starts_with() already implemented"
+#endif
+
+inline bool startsWith(const StringDecl& str, const StringDecl& start)
+{
+#if defined(ESP32)
+    return str.size() >= start.size() and
+           str.substr(0, start.size()) == start;
+#else
+    return str.startsWith(start);
+#endif
+}
+
+inline StringDecl substring(const StringDecl& str, unsigned int beginIndex)
+{
+#if defined(ESP32)
+    return str.substr(beginIndex);
+#else
+    return str.substring(beginIndex);
+#endif
+}
+
+inline StringDecl substring(const StringDecl& str, unsigned int beginIndex, unsigned int endIndex)
+{
+#if defined(ESP32)
+    return str.substr(beginIndex, endIndex);
+#else
+    return str.substring(beginIndex, endIndex);
+#endif
+}
+
+inline int indexOf(const StringDecl& str, char match)
+{
+#if defined(ESP32)
+    return str.find(match);
+#else
+    return str.indexOf(match);
+#endif
+}
+
+inline int indexOf(const StringDecl& str, const StringDecl& match)
+{
+#if defined(ESP32)
+    return str.find(match);
+#else
+    return str.indexOf(match);
+#endif
+}
