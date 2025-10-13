7ad6a5ceb159140a86bca9f472d93c790c1b75a8
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2025-10-13 10:27:56 +0200

    Using simpler macros

--- examples/multiservice/multiservice.ino
+++ examples/multiservice/multiservice.ino
@@ -19,7 +19,7 @@ void onWrite(BLEDevice central, BLECharacteristic characteristic) {
 }
 #endif
 
-#ifdef BLE_CHESS_BLE_LIB_NIM_BLE_ARDUINO
+#ifdef BLE_CHESS_LIB_NIM_BLE_ARDUINO
 class LedCharacteristicCallbacks: public BLECharacteristicCallbacks {
 public:
 #ifdef BLE_CHESS_BLE_LIB_NIM_BLE_ARDUINO_V1
@@ -53,7 +53,7 @@ void setup() {
   advertiseBle(NAME, BLE_CHESS_SERVICE_UUID, LED_SERVICE_UUID);
 #endif
 
-#ifdef BLE_CHESS_BLE_LIB_NIM_BLE_ARDUINO
+#ifdef BLE_CHESS_LIB_NIM_BLE_ARDUINO
   auto server = initBle(NAME);
   auto ledService = server->createService(LED_SERVICE_UUID);
   auto ledCharacteristic = ledService->createCharacteristic(
